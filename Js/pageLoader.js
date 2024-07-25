ad66a09ccc5e6fe652ef10e399c85b085f0e75ba
Esteban Bentancor <ebentancor341@gmail.com>
2024-07-25 02:50:15 -0300

    v 0.1
    v 0.1

---
+++ Js/pageLoader.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loader = document.getElementById('terminal-loader');
+    document.body.style.display = 'block';
+    loader.style.display = 'none';
+    alert('funciona')
+  });
